b55aa3012142bf6f49a4678ecf1159036eaec248
dean <dies0mbre@gmail.com>
2019-12-08 18:04:31 +0300

    second one

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ package com.example.kotlinserver
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+val port = System.getenv("PORT")?.toInt() ?: 23567
+
 
 fun main(args : Array<String>){
     embeddedServer(Netty, commandLineEnvironment(args)).start(wait = true)
